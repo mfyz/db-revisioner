122b343d30730fbfb8cb7234383d3b372417fa7c
Mehmet Fatih YILDIZ <fatih@mfyz.com>
2013-04-06 00:11:22 -0400

    initial commit

---
+++ config-sample.php
@@ -0,0 +1,7 @@
+<?php
+
+define("CONFIG_HOST",         'localhost');
+define("CONFIG_USERNAME",     'root');
+define("CONFIG_PASSWORD",     'root_password');
+define("CONFIG_DATABASE",     'database_name');
+define("CONFIG_VERSIONS_DIR", 'versions');
